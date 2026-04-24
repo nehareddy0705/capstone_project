bb1bcdc41ba0f6103b206d8dcb4ba2d0ad0a7c4c
nehareddy0705 <mittabaineha0705@gmail.com>
2026-04-24 15:36:14 +0530

    latest commit

--- blog-frontend/src/components/Home.jsx
+++ blog-frontend/src/components/Home.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
+// src/components/Home.jsx
 
+const BASE_URL = "https://capstone-project-blog-app-0eap.onrender.com";
+
+useEffect(() => {
+  fetch(`${BASE_URL}/api/posts`)
+    .then(res => res.json())
+    .then(data => console.log(data));
+}, []);
 function Home() {
   return (
     <div>Home</div>
